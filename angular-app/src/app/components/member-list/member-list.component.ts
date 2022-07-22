1ce99a4b922089e92ab56711dccd1b0cb9410097
brainspork <anthony.konschuh@gmail.com>
2022-07-22 16:45:51 -0700

    Add pagination

--- angular-app/src/app/components/member-list/member-list.component.ts
+++ angular-app/src/app/components/member-list/member-list.component.ts
@@ -10,8 +10,13 @@ import { IMemberApiService } from 'src/app/services/api/member/member-api.servic
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
+  private maxPerPage = 4;
+
+  public numberOfPages = 1;
+  public currentPage: BehaviorSubject<number> = new BehaviorSubject(1);
 
   public members: BehaviorSubject<IMemberDto[]> = new BehaviorSubject<IMemberDto[]>([]);
+  public visibleMembers: BehaviorSubject<IMemberDto[]> = new BehaviorSubject<IMemberDto[]>([]);
 
   constructor(
     @Inject(IMemberApiServiceProvider)
@@ -20,6 +25,16 @@ export class MemberListComponent implements OnInit {
 
   ngOnInit(): void {
     this.getUsers();
+
+    this.members.subscribe(members => {
+      this.numberOfPages = Math.ceil(members.length / this.maxPerPage) || 1;
+      this.currentPage.next(this.currentPage.value > this.maxPerPage ? this.maxPerPage : this.currentPage.value);
+    });
+
+    this.currentPage.subscribe(page => {
+      const startIndex = page === 1 ? 0 : (page - 1) * this.maxPerPage;
+      this.visibleMembers.next(this.members.value.slice(startIndex, startIndex + this.maxPerPage))
+    });
   }
 
   deleteUser(id: number): void {
@@ -29,5 +44,17 @@ export class MemberListComponent implements OnInit {
 
   getUsers() {
     this.apiService.getCollection().subscribe(members => this.members.next(members));
+  }
+
+  next() {
+    if (this.currentPage.value < this.numberOfPages) {
+      this.currentPage.next(this.currentPage.value + 1);
+    }
+  }
+
+  prev() {
+    if (this.currentPage.value > 1) {
+      this.currentPage.next(this.currentPage.value - 1);
+    }
   }
 }
